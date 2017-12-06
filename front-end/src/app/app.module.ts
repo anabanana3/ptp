3928ac134b2e8130e33f170d4205549484856be2
Andrea Macia <andreamacia@MacBook-Pro-de-Andrea.local>
2017-12-06 20:22:50 +0100

    desplegable asociaciones y profesiones

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {APP_ROUTING} from "./app.routes";
 
-
+//Components
 import { AppComponent } from './app.component';
-import { RegistroComponent } from './registro/registro.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { KeysPipe } from './pipes/keys.pipe';
 
+//Services
+import {ProfesionesService} from "./services/profesiones.service";
+import {AsociacionesService} from "./services/asociaciones.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistroComponent
+    RegistroComponent,
+    KeysPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    ProfesionesService,
+    AsociacionesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
