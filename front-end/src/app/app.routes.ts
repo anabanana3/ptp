0196ad13e9c93b6d02f3b006bfc817c67769d019
Clara <clarasaquero8@gmail.com>
2017-12-15 17:47:50 +0100

    arreglando lateraloptions

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -5,11 +5,8 @@ import { AsociacionesComponent } from './components/admin/asociaciones.component
 import { RegistroLoginComponent } from './components/registro/registroLogin.component';
 import { HomeComponent } from './components/user/home/home.component';
 import { HomeAsociaciones } from './components/asociaciones/homeAsociaciones.component';
-<<<<<<< HEAD
 import {LoginComponent} from './components/login/login.component';
-=======
 import { LateralOptionsComponent } from './components/admin/lateralOptions/lateralOptions.component';
->>>>>>> 3bccd87fb0586c446c14f548fa5bcd7141d14ddd
 
 const app_routes: Routes = [
   { path: 'usuarios',
@@ -46,11 +43,13 @@ const app_routes: Routes = [
     component: HomeAsociaciones,
     data: { title: 'Home' }
   },
+
   {
-    path: 'options',
-    component: LateralOptionsComponent,
-    data: { title: 'options' }
-  },
+  path: 'options',
+  component: LateralOptionsComponent,
+  data: { title: 'options' }
+ },
+
   { path: '**', pathMatch: 'full', redirectTo: 'registro' }
 ];
 
