c66db665b6e44eea5ff4dc71a2241c20dd98aff3
Andrea Macia <andreita-ama@hotmail.com>
2018-02-14 18:59:39 +0100

    Añadir Recurso funcionando // getRecursos tamb

--- front-end/src/app/components/recursos/recursos.component.ts
+++ front-end/src/app/components/recursos/recursos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MaterialService } from "../../services/material.service";
+import {Recurso} from '../../interfaces/recurso.interface';
 
 @Component({
   selector: 'app-recursos',
@@ -7,11 +9,18 @@ import { Component } from '@angular/core';
 export class RecursosComponent {
 
   error:boolean = true;
-  constructor() {
+  recursos = [];
+  constructor(private _materialService:MaterialService) {
     if(sessionStorage.length === 0){
       return;
     }
     this.error = false;
-  }
 
+    _materialService.getMateriales().subscribe(data => {
+      this.recursos = data;
+      console.log(this.recursos);
+    }, error => {
+      console.log(error);
+    })
+  }
 }
--- front-end/src/app/services/material.service.ts
+++ front-end/src/app/services/material.service.ts
@@ -6,11 +6,24 @@ import 'rxjs/add/operator/map'; //no lo dice en los tutoriales->para que funcion
 @Injectable()
 export class MaterialService {
   nuevoMaterial:string = "https://aisha.ovh/api/material/";
+  materiales:string = "https://aisha.ovh/api/material/";
+  materialesPublicos:string = "https://aisha.ovh/api/material/publicos";
+  formatos:string = "https://aisha.ovh/api/formato/";
 
   constructor(private http:Http) { }
 
-  newMaterial(material:Recurso){
-    let body = JSON.stringify(material);
+  newMaterial(material:FormData){
+    let body = material;
+    let token = sessionStorage.getItem('token');
+    let url = this.nuevoMaterial + token;
+
+    return this.http.post(url, body).map(res=>{
+            console.log(res.json());
+            return res.json();
+          })
+  }
+
+  getFormatos(){
     let token = sessionStorage.getItem('token');
 
     let headers = new Headers({
@@ -18,9 +31,24 @@ export class MaterialService {
       'Authorization': token
     });
 
-    return this.http.post(this.nuevoMaterial, body, {headers}).map(res=>{
-            console.log(res.json());
-            return res.json();
-          })
+    return this.http.get(this.formatos, {headers}).map(res=>res.json());
+  }
+
+  getMateriales(){
+    let token = sessionStorage.getItem('token');
+    let headers = new Headers({
+      'Content-Type':'application/json',
+      'Authorization': token
+    });
+
+    return this.http.get(this.materiales, {headers}).map(res=>res.json());
+  }
+
+  getMaterialesPublicos(){
+    let headers = new Headers({
+      'Content-Type':'application/json',
+    });
+
+    return this.http.get(this.materialesPublicos, {headers}).map(res=>res.json());
   }
 }
