6b55b3103c86a8f3b0f6bded50c742c1ba76123c
Unknown <carlos_garcia15@hotmail.es>
2018-03-09 14:40:13 +0100

    Corrigiendo conflictos

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -32,7 +32,7 @@ import { ContactoComponent } from './components/contacto/contacto.component';
 import { FooterComponent } from './footer/footer.component';
 import { VermasComponent } from './components/vermas/vermas.component';
 import { Bloque1Component } from './components/expediente/bloque1/bloque1.component';
-import { Bloque2Component } from './components/expediente/bloque2/bloque2.component';
+import { Bloque2Component, Popup2 } from './components/expediente/bloque2/bloque2.component';
 import { Bloque3Component } from './components/expediente/bloque3/bloque3.component';
 import { Bloque4Component } from './components/expediente/bloque4/bloque4.component';
 import { Bloque5Component } from './components/expediente/bloque5/bloque5.component';
@@ -98,10 +98,7 @@ import { EstadisticasComponent } from './components/estadisticas/estadisticas.co
     ExpedientesAdminComponent,
     Popup,
     Popup2,
-    ChatComponent
-
-
-
+    ChatComponent,
     EstadisticasComponent
   ],
   imports: [
